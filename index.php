22d928af1269c2467f4c017c2cf9a285b029d7ff
Michaela McCann <kaebot@me.com>
2015-01-15 11:23:49 -0800

    broke out office hours logic

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ $artists_header	  = get_option('mac_settings')['mac_talent_header'];
 						while ($artist_query->have_posts()) {
 							$artist_query->the_post();
 
-							include 'artist_front_page.php';
+							include 'artist_unit.php';
 						}
 					}
 
@@ -129,7 +129,8 @@ $artists_header	  = get_option('mac_settings')['mac_talent_header'];
 					if ($feat_artist_query->have_posts()) {
 						while ($feat_artist_query->have_posts()) {
 							$feat_artist_query->the_post();
-							include 'artist_front_page.php';
+							
+							include 'artist_unit.php';
 						}
 					}
 
@@ -155,48 +156,7 @@ $artists_header	  = get_option('mac_settings')['mac_talent_header'];
 						while ($oh_artist_query->have_posts()) {
 							$oh_artist_query->the_post();
 							
-							//if it's the first item
-							if ($i == -4){
-								?>
-								<div class="artist_unit">
-									<div class="headers" id="office-hours">
-										<h1><span>Plus</span><br>Office Hours<br><span>With</span></h1>
-									</div>
-									<div class="artists-name artist-office">
-										<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-									</div>
-									<?php $i++;
-							//if it's the last item *and* a multiple of 8 (or 0), start and close the div
-							} else if ( ($i % 8 == 0 || $i ==0) && ($i == ($oh_count-5))){ ?>
-								</div>
-								<div class="artist_unit">
-									<div class="artists-name artist-office">
-										<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-									</div>
-								</div>
-								<?php $i++;
-							// if it's a multiple of 8 or 0, close the prev div, start a new one
-							} else if ($i % 8 == 0 || $i ==0){ ?>
-								</div>
-								<div class="artist_unit">
-									<div class="artists-name artist-office">
-										<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-									</div>
-								<?php $i++;
-							//if it's the last item on the list, close the tag
-							} else if ($i == ($oh_count-5)){ ?>
-									<div class="artists-name artist-office">
-										<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-									</div>
-								</div>
-								<?php $i++;
-							//otherwise just add a name to the list
-							} else { ?>
-									<div class="artists-name artist-office">
-										<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-									</div>
-								<?php $i++; ?>
-					<?php	}
+							include 'artist_office_hours_logic.php';
 						}
 					}
 					wp_reset_postdata();
