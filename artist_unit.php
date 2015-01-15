7dd4c739b9f1663fd0c80c514fa185b3447ac066
Michaela McCann <kaebot@me.com>
2015-01-15 12:43:43 -0800

    added targets to open social links in new window

--- artist_unit.php
+++ artist_unit.php
@@ -11,15 +11,15 @@
 	</div>
 	<div class="artists-social">
 	<?php  if (get_field('artist_facebook')){ ?>
-			<a href="<?php echo get_field('artist_facebook'); ?>" class="social-icon facebook">
+			<a href="<?php echo get_field('artist_facebook'); ?>" class="social-icon facebook" target="_blank">
 			</a>
 		<? };
 		   if (get_field('artist_twitter')){ ?>
-			<a href="<?php echo get_field('artist_twitter'); ?>"  class="social-icon twitter">
+			<a href="<?php echo get_field('artist_twitter'); ?>"  class="social-icon twitter" target="_blank">
 			</a>
 		<? };
 		   if (get_field('artist_youtube')){ ?>
-			<a class="social-icon youtube" href="<?php echo get_field('artist_youtube'); ?>" class="social-icon youtube">
+			<a class="social-icon youtube" href="<?php echo get_field('artist_youtube'); ?>" class="social-icon youtube" target="_blank">
 			</a>
 		<? }; ?>
 		
