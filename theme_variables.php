ac322e41a6ff365961021612a6436db4cbf85d96
Michaela McCann <kaebot@me.com>
2015-01-20 15:42:46 -0800

    fixed formatting

--- theme_variables.php
+++ theme_variables.php
@@ -1,19 +1,19 @@
 <?php 
 //variables
-$site_title 	  = get_bloginfo('name'); 
-$booking_url	  = get_option('mac_settings')['mac_booking_url'];
+$site_title 	  	  = get_bloginfo('name'); 
+$booking_url	  	  = get_option('mac_settings')['mac_booking_url'];
 $booking_enabled  = get_option('mac_settings')['mac_booking_enabled'];
-$booking_cta	  = get_option('mac_settings')['mac_button_cta'];
+$booking_cta	  	  = get_option('mac_settings')['mac_button_cta'];
 $cruise_fb	  	  = get_option('mac_settings')['mac_facebook_url'];
 $cruise_twitter	  = get_option('mac_settings')['mac_twitter_url'];
 $cruise_rss		  = get_option('mac_settings')['mac_feed_url'];
-$travel_desc	  = get_option('mac_settings')['mac_travel_description'];
+$travel_desc	  	  = get_option('mac_settings')['mac_travel_description'];
 $travel_desc_more = get_option('mac_settings')['mac_travel_description_more'];
-$mailing_cta	  = get_option('mac_settings')['mac_mailing_list_cta'];
+$mailing_cta	  	  = get_option('mac_settings')['mac_mailing_list_cta'];
 $artists_header	  = get_option('mac_settings')['mac_talent_header'];
 $cont_gen_q		  = get_option('mac_settings')['mac_general_questions_header'];
 $cont_gen_q_addy  = get_option('mac_settings')['mac_general_questions_address_header'];
-$cont_book_q	  = get_option('mac_settings')['mac_booking_questions_header'];
+$cont_book_q	  	  = get_option('mac_settings')['mac_booking_questions_header'];
 $cont_book_q_addy = get_option('mac_settings')['mac_booking_questions_address_header'];
 $cont_tel		  = get_option('mac_settings')['mac_phone_questions_header'];
 $cont_tel_addy	  = get_option('mac_settings')['mac_phone_questions_address_header'];
