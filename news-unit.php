3e032b65e58362422e60195738662382450ed3fc
Michaela McCann <kaebot@me.com>
2015-01-28 16:32:18 -0800

    added 'more' support

--- news-unit.php
+++ news-unit.php
@@ -1,8 +1,8 @@
-<a class="news-item-title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+<a class="news-item-title" href="<?php the_permalink(); ?>"><span><?php the_title(); ?></span></a>
 <?php
 if($post->post_excerpt){
 	the_excerpt();
 } else {
-	the_content();
+	the_content('more&hellip;');
 }
 ?>
