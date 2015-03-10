a1bcb0500f4b2ac57832b0b400e76d6b253b2069
Drew Westphal <drewwestphal@users.noreply.github.com>
2015-03-10 13:59:51 -0400

    auto fmt theme settings

--- theme_variables.php
+++ theme_variables.php
@@ -1,25 +1,26 @@
-<?php 
-//variables
-$site_title 	  	  = get_bloginfo('name'); 
-$booking_url	  	  = get_option('mac_settings')['mac_booking_url'];
-$booking_enabled  = get_option('mac_settings')['mac_booking_enabled'];
-$booking_cta	  	  = get_option('mac_settings')['mac_button_cta'];
-$cruise_fb	  	  = get_option('mac_settings')['mac_facebook_url'];
-$cruise_twitter	  = get_option('mac_settings')['mac_twitter_url'];
-$cruise_rss		  = get_option('mac_settings')['mac_feed_url'];
-$travel_desc	  	  = get_option('mac_settings')['mac_travel_description'];
-$travel_desc_more = get_option('mac_settings')['mac_travel_description_more'];
-$mailing_cta	  	  = get_option('mac_settings')['mac_mailing_list_cta'];
-$artists_header	  = get_option('mac_settings')['mac_talent_header'];
-$cont_gen_q		  = get_option('mac_settings')['mac_general_questions_header'];
-$cont_gen_q_addy  = get_option('mac_settings')['mac_general_questions_address_header'];
-$cont_book_q	  	  = get_option('mac_settings')['mac_booking_questions_header'];
-$cont_book_q_addy = get_option('mac_settings')['mac_booking_questions_address_header'];
-$cont_tel		  = get_option('mac_settings')['mac_phone_questions_header'];
-$cont_tel_addy	  = get_option('mac_settings')['mac_phone_questions_address_header'];
-$map_copy		  = get_option('mac_settings')['mac_map_copy'];
-$news_header		  = get_option('mac_settings')['mac_news_header'];
-$news_view_all	  = get_option('mac_settings')['mac_news_view_all'];
-$news_view_url	  = get_option('mac_settings')['mac_news_view_all_url'];
+<?php
 
+$settings = $settings;
+//variables
+$site_title = get_bloginfo('name');
+$booking_url = $settings['mac_booking_url'];
+$booking_enabled = $settings['mac_booking_enabled'];
+$booking_cta = $settings['mac_button_cta'];
+$cruise_fb = $settings['mac_facebook_url'];
+$cruise_twitter = $settings['mac_twitter_url'];
+$cruise_rss = $settings['mac_feed_url'];
+$travel_desc = $settings['mac_travel_description'];
+$travel_desc_more = $settings['mac_travel_description_more'];
+$mailing_cta = $settings['mac_mailing_list_cta'];
+$artists_header = $settings['mac_talent_header'];
+$cont_gen_q = $settings['mac_general_questions_header'];
+$cont_gen_q_addy = $settings['mac_general_questions_address_header'];
+$cont_book_q = $settings['mac_booking_questions_header'];
+$cont_book_q_addy = $settings['mac_booking_questions_address_header'];
+$cont_tel = $settings['mac_phone_questions_header'];
+$cont_tel_addy = $settings['mac_phone_questions_address_header'];
+$map_copy = $settings['mac_map_copy'];
+$news_header = $settings['mac_news_header'];
+$news_view_all = $settings['mac_news_view_all'];
+$news_view_url = $settings['mac_news_view_all_url'];
 ?>
