b6426e6f5f9b6868a36e91fcd6c2467094a6df58
Michaela McCann <kaebot@me.com>
2015-03-18 11:40:21 -0700

    updated class for theme

--- page.php
+++ page.php
@@ -30,10 +30,10 @@
     ?> 
     
     
-    <section class="mac-page" id="page-page">
+    <section class="joco-page" id="page-page">
         <div class="container">
             <div class="col-xs-12 col-md-12">
-                <h1 class="orange-text"><?php the_title() ?></h1>
+                <h1><?php the_title() ?></h1>
                 <?php if (have_posts()) : while (have_posts()) : the_post();?>
                     <p class="mac-page-intro"><?php the_content(); ?></p>
                 <?php endwhile; endif; ?>
