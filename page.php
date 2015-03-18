edef02361161c4d32396ebab63b0df9dcf181f8d
Michaela McCann <kaebot@me.com>
2015-03-18 11:42:35 -0700

    added back to site element

--- page.php
+++ page.php
@@ -31,13 +31,23 @@
     
     
     <section class="joco-page" id="page-page">
+		<div class="bumper container-fluid">
+			<div class="bumper-container">
+				<div class="bumper-element bumper-left" id="bumper-home">
+					<div class="bumper-container">
+						<span class="glyphicon glyphicon-menu-left"></span>
+						<a href="/">Back to Site</a>
+					</div>
+				</div>
+			</div>
+		</div>
         <div class="container">
             <div class="col-xs-12 col-md-12">
                 <h1><?php the_title() ?></h1>
                 <?php if (have_posts()) : while (have_posts()) : the_post();?>
                     <p class="mac-page-intro"><?php the_content(); ?></p>
                 <?php endwhile; endif; ?>
-                <?php wp_footer(); ?>   
+                <?php wp_footer(); ?> 
             </div>
         </div>
     </section>
