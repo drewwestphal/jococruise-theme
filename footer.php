99929f22ce9df4da30169d9e2d16e29f99e042eb
Drew Westphal <drewwestphal@users.noreply.github.com>
2015-03-27 22:06:54 -0400

    allow footer text to be unset

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 </div>
 <footer>
 	<?php $settings = get_option('mac_settings'); ?>
-	<?php $footer_text = $settings['mac_footer_text']; ?>
+	<?php $footer_text = @$settings['mac_footer_text']; ?>
 	<?php include 'sponsors.php'; ?>
 	<div class="footer-info">
 		<small>&copy;<?php echo date("Y"); ?> JoCoCruise<?php if (strlen($footer_text)>0){ echo ' '.$footer_text; }; ?></small>
