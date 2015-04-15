3524b3078d7544a49b06508767ffd529d512e369
David Mackenzie <davidamackenzie@gmail.com>
2015-04-15 19:05:39 -0400

    Added custom 404 page

--- 404.php
+++ 404.php
@@ -1,6 +1,21 @@
 <?php get_header(); ?>
 <section id="content" role="main">
-<article id="post-0" class="post not-found">
+    	<div style="height: 50px;"><!--PADDING--></div>
+	    <section class="joco-page" id="page-page">
+		<?php include 'bumper_top.php'; ?>
+        <div class="container headers">
+            <div class="col-xs-4 col-md-4">
+                <img src="<?php bloginfo('template_directory'); ?>/img/boat_sideways.png" class="failboat" />
+            </div>
+            <div class="col-xs-8 col-md-8">
+                <p class="text404">404: Lost at sea!</p>
+            </div>
+        </div>
+        <?php include 'bumper_bottom.php'; ?>
+        <?php get_footer(); ?>
+    </section>
+    
+<!--<article id="post-0" class="post not-found">
 <header class="header">
 <h1 class="entry-title"><?php _e( 'Not Found', 'blankslate' ); ?></h1>
 </header>
@@ -11,4 +26,4 @@
 </article>
 </section>
 <?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php get_footer(); ?>-->
