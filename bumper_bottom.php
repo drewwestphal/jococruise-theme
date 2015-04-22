1fbb7a3d7774f6d2be13ab90d8ba6c3a587db193
David Mackenzie <davidamackenzie@gmail.com>
2015-04-22 16:43:58 -0400

    Restored next and prev links in single news page

--- bumper_bottom.php
+++ bumper_bottom.php
@@ -1,18 +1,18 @@
-<!--
+<?php if ( in_category( 'latest-news' )  ) {  ?>
 <div class="bumper bumper-bottom container-fluid">
 	<div class="bumper-container">
 			<div class="bumper-element bumper-left" id="bumper-left">
-				<?php if (get_previous_post()->ID) { ?>
+				<?php if (get_next_post()->ID) { ?>
 					<span class="glyphicon glyphicon-menu-left"></span>
-					<div class="bumper-a-container"><a href="<?php echo get_permalink( get_previous_post()->ID );?>"><?php echo get_previous_post()->post_title;?></a></div>
+					<div class="bumper-a-container"><a href="<?php echo get_permalink( get_next_post()->ID );?>"><?php echo get_next_post()->post_title;?></a></div>
 				<?php }; ?>
 			</div>
 			<div class="bumper-element bumper-right" id="bumper-right">
-				<?php if (get_next_post()->ID) { ?>
-					<div class="bumper-a-container"><a href="<?php echo get_permalink( get_next_post()->ID );?>"><?php echo get_next_post()->post_title;?></a></div>
+				<?php if (get_previous_post()->ID) { ?>
+					<div class="bumper-a-container"><a href="<?php echo get_permalink( get_previous_post()->ID );?>"><?php echo get_previous_post()->post_title;?></a></div>
 					<span class="glyphicon glyphicon-menu-right"></span>
 				<?php }; ?>
 			</div>
 	</div>
 </div>
--->
+<?php }; ?>
