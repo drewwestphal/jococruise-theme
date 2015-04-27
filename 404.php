1ef3b132408f9cb33ccb70361ceba5a2c1a5c8d6
David Mackenzie <davidamackenzie@gmail.com>
2015-04-27 15:28:37 -0400

    Jessie 404 page design

--- 404.php
+++ 404.php
@@ -4,16 +4,29 @@
 	    <section class="joco-page" id="page-page">
 		<?php include 'bumper_top.php'; ?>
         <div class="container headers">
-            <div class="col-xs-12 col-md-4" style="text-align: center;">
-                <img src="<?php bloginfo('template_directory'); ?>/img/boat_sideways.png" class="failboat" />
+            <div class="col-xs-12 col-md-12" style="text-align: center;">
+                <img src="<?php bloginfo('template_directory'); ?>/img/JoCo_dophinanchor.png" class="failboat" />
             </div>
-            <div class="col-xs-12 col-md-8">
-                <p class="text404">404: Lost at sea!</p>
+            <div class="col-xs-12 col-md-12 text404">
+                <!--<p class="text404">404: Lost at sea!</p>-->
+            	<h1 class="header404">Something went overboard.</h1>
+            	<p>It appears that what you're looking for has been lost at sea.
+            		<br /><br />
+            		We'll send out the lifeboats, but in the meantime, 
+            		<br />can we interest you in something on deck?
+            	</p>
+            	<div class="button404">
+            		<a href="/">Back to the main page</a>
+            	</div>
             </div>
         </div>
         <?php include 'bumper_bottom.php'; ?>
         <?php get_footer(); ?>
     </section>
+    
+<script type="text/javascript">
+    jQuery('.button404').click(function(){jQuery(this).find('a')[0].click();});
+</script>
     
 <!--<article id="post-0" class="post not-found">
 <header class="header">
