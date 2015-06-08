61bea0959e039e3082f0a7f32a407fd90777090e
Drew Westphal <drewwestphal@users.noreply.github.com>
2015-06-08 12:07:09 -0400

    fix notice NOTICE: PHP message: PHP Strict Standards:  Only variables should be passed by reference in /var/www/html/jcc/themes/jococruise-theme/functions.php on line 239

--- functions.php
+++ functions.php
@@ -236,7 +236,7 @@ function blankslate_comments_number( $count )
 {
 if ( !is_admin() ) {
 global $id;
-$comments_by_type = &separate_comments( get_comments( 'status=approve&post_id=' . $id ) );
+$comments_by_type = separate_comments( get_comments( 'status=approve&post_id=' . $id ) );
 return count( $comments_by_type['comment'] );
 } else {
 return $count;
