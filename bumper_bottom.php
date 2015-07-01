0dd7f924b16576ca85fae1e69ba6d81db83150ef
David Mackenzie <davidamackenzie@gmail.com>
2015-07-01 15:13:25 -0400

    Bumper navigation styling for news pages

--- bumper_bottom.php
+++ bumper_bottom.php
@@ -1,18 +1,22 @@
 <?php if ( in_category( 'latest-news' )  ) {  ?>
-<div class="bumper bumper-bottom container-fluid">
-	<div class="bumper-container">
-			<div class="bumper-element bumper-left" id="bumper-left">
-				<?php if (get_next_post()->ID) { ?>
+<div class="bumper bumper-bottom">
+	<div class="container">
+		<div class="col-xs-6 bumper-left">
+			<?php if (isset(get_next_post()->ID)) { ?>
+				<a href="<?php echo get_permalink( get_next_post()->ID );?>">
 					<span class="glyphicon glyphicon-menu-left"></span>
-					<div class="bumper-a-container"><a href="<?php echo get_permalink( get_next_post()->ID );?>"><?php echo get_next_post()->post_title;?></a></div>
-				<?php }; ?>
-			</div>
-			<div class="bumper-element bumper-right" id="bumper-right">
-				<?php if (get_previous_post()->ID) { ?>
-					<div class="bumper-a-container"><a href="<?php echo get_permalink( get_previous_post()->ID );?>"><?php echo get_previous_post()->post_title;?></a></div>
+					<?php echo get_next_post()->post_title;?>
+				</a>
+			<?php }; ?>
+		</div>
+		<div class="col-xs-6 bumper-right">
+			<?php if (isset(get_previous_post()->ID)) { ?>
+				<a href="<?php echo get_permalink( get_previous_post()->ID );?>">
+					<?php echo get_previous_post()->post_title;?>
 					<span class="glyphicon glyphicon-menu-right"></span>
-				<?php }; ?>
-			</div>
+				</a>
+			<?php }; ?>
+		</div>
 	</div>
 </div>
 <?php }; ?>
