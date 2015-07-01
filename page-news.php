5041a0056acfae9557bed59bd65856527dacac0a
David Mackenzie <davidamackenzie@gmail.com>
2015-07-01 15:33:18 -0400

    All news page styling

--- page-news.php
+++ page-news.php
@@ -1,35 +1,30 @@
 <?php get_header(); ?>
-<section id="content" role="main">
 <?php include 'bumper_top.php'; ?>
-<section class="mac-page headers" id="page-news">
-	<div class="container-fluid">
-		<div class="col-xs-12 col-md-12">
-			<h1 class="orange-text">News</h1>
-		    <?php if (have_posts()) : while (have_posts()) : the_post();?>
-	            <p class="mac-page-intro"><?php the_content(); ?></p>
-		    <?php endwhile; endif; ?>
+<div class="container nav-spacer">
+	<div class="col-xs-12">
+		<h1>News</h1>
+	    <?php if (have_posts()) : while (have_posts()) : the_post();?>
+            <p><?php the_content(); ?></p>
+	    <?php endwhile; endif; ?>
 		<?php 
-			$args = array(
-				'post_type' => 'post'
-			);
-			$post_query = new WP_Query( $args );
-			if ( $post_query->have_posts() ) {
-				while ( $post_query->have_posts() ) {
-					$post_query->the_post();
-					?>
-						<article id="post-artist-info">
-							<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-						<?php if ($post->post_content) {?>
-							<div class="post-artist-text"><?php the_content(); ?></div>
-						<?php  } else { ?>
-							<div class="post-artist-text"><?php the_excerpt(); ?></div>
-						<?php  }; ?>	
-						</article>
-				<?php  }; ?>	
-			<?php  }; ?>
-			
-		</div>
+		$args = array(
+			'post_type' => 'post'
+		);
+		$post_query = new WP_Query( $args );
+		if ( $post_query->have_posts() ) {
+			while ( $post_query->have_posts() ) {
+				$post_query->the_post();
+				?>
+					<article class="news-page-item">
+						<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					<?php if ($post->post_content) {?>
+						<div><?php the_content(); ?></div>
+					<?php  } else { ?>
+						<div><?php the_excerpt(); ?></div>
+					<?php  }; ?>	
+					</article>
+			<?php  }; ?>	
+		<?php  }; ?>
 	</div>
-</section>
-<?php include 'bumper_bottom_empty.php'; ?>
+</div>
 <?php get_footer(); ?>	
