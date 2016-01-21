009ac4565eef0d92acf6ee54985c165af0f7da83
Drew Westphal <drewwestphal@users.noreply.github.com>
2016-01-20 19:15:58 -0500

    --amend

--- news-unit.php
+++
@@ -1,7 +0,0 @@
-<a class="news-item-title" href="<?php the_permalink(); ?>"><span><?php the_title(); ?></span></a>
-<?php if($post->post_excerpt){
-	the_excerpt();
-} else {
-	the_content('more&hellip;');
-}
-?>
