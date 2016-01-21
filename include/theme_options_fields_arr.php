e93292a1b3ca541d9c82f1c97d450fcb97b16356
Drew Westphal <drewwestphal@users.noreply.github.com>
2016-01-21 01:16:59 -0500

    fmt options

--- include/theme_options_fields_arr.php
+++ include/theme_options_fields_arr.php
@@ -1,236 +1,236 @@
 <?php
-return array(
-    array(
+return [
+    [
         'name' => 'Sitewide Options',
         'desc' => '',
         'type' => 'title',
-        'id' => 'mac_site_settings'
-    ),
-    array(
-        'name' => 'Cruise Year',
-        'desc' => '',
-        'id' => 'mac_cruise_year',
-        'type' => 'text_small',
+        'id'   => 'mac_site_settings',
+    ],
+    [
+        'name'    => 'Cruise Year',
+        'desc'    => '',
+        'id'      => 'mac_cruise_year',
+        'type'    => 'text_small',
         'default' => false,
-    ),
-    array(
-        'name' => 'Talent Year',
-        'desc' => 'Which year worth of talent should we load on the front page?',
-        'id' => 'mac_talent_year',
-        'type' => 'text_small',
+    ],
+    [
+        'name'    => 'Talent Year',
+        'desc'    => 'Which year worth of talent should we load on the front page?',
+        'id'      => 'mac_talent_year',
+        'type'    => 'text_small',
         'default' => false,
-    ),
-    array(
-        'name' => 'Enable booking',
-        'desc' => '',
-        'id' => 'mac_enable_booking',
-        'type' => 'checkbox',
+    ],
+    [
+        'name'    => 'Enable booking',
+        'desc'    => '',
+        'id'      => 'mac_enable_booking',
+        'type'    => 'checkbox',
         'default' => false,
-    ),
-    array(
-        'name' => 'Booking Path',
-        'desc' => 'Root relative URL for booking site',
-        'id' => 'mac_booking_url',
-        'type' => 'text_medium',
+    ],
+    [
+        'name'    => 'Booking Path',
+        'desc'    => 'Root relative URL for booking site',
+        'id'      => 'mac_booking_url',
+        'type'    => 'text_medium',
         'default' => false,
-    ),
-    array(
-        'name' => 'Contact Form Email Address',
-        'desc' => '',
-        'id' => 'mac_contact_email',
-        'type' => 'text_email',
+    ],
+    [
+        'name'    => 'Contact Form Email Address',
+        'desc'    => '',
+        'id'      => 'mac_contact_email',
+        'type'    => 'text_email',
         'default' => false,
-    ),
-    array(
+    ],
+    [
         'name' => 'Hero Section Options',
         'desc' => '',
         'type' => 'title',
-        'id' => 'mac_hero_settings'
-    ),
-    array(
-        'name' => 'Travel Dates',
-        'desc' => '',
-        'id' => 'mac_travel_dates',
-        'type' => 'textarea_small',
+        'id'   => 'mac_hero_settings',
+    ],
+    [
+        'name'    => 'Travel Dates',
+        'desc'    => '',
+        'id'      => 'mac_travel_dates',
+        'type'    => 'textarea_small',
         'default' => false,
-    ),
-    array(
-        'name' => 'Button Call to Action',
-        'desc' => '',
-        'id' => 'mac_button_cta',
-        'type' => 'text_medium',
+    ],
+    [
+        'name'    => 'Button Call to Action',
+        'desc'    => '',
+        'id'      => 'mac_button_cta',
+        'type'    => 'text_medium',
         'default' => false,
-    ),
-    array(
-        'name' => 'Travel Description',
-        'desc' => '',
-        'id' => 'mac_travel_description',
-        'type' => 'textarea',
+    ],
+    [
+        'name'    => 'Travel Description',
+        'desc'    => '',
+        'id'      => 'mac_travel_description',
+        'type'    => 'textarea',
         'default' => false,
-    ),
-    array(
-        'name' => 'Travel Description (More)',
-        'desc' => '',
-        'id' => 'mac_travel_description_more',
-        'type' => 'textarea',
+    ],
+    [
+        'name'    => 'Travel Description (More)',
+        'desc'    => '',
+        'id'      => 'mac_travel_description_more',
+        'type'    => 'textarea',
         'default' => false,
-    ),
-    array(
-        'name' => 'Hero Book Now Button',
-        'desc' => '',
-        'id' => 'mac_hero_book_now',
-        'type' => 'text_medium',
+    ],
+    [
+        'name'    => 'Hero Book Now Button',
+        'desc'    => '',
+        'id'      => 'mac_hero_book_now',
+        'type'    => 'text_medium',
         'default' => false,
-    ),
-    array(
-        'name' => 'Hero Already Booked Button',
-        'desc' => '',
-        'id' => 'mac_hero_already_booked',
-        'type' => 'text_medium',
+    ],
+    [
+        'name'    => 'Hero Already Booked Button',
+        'desc'    => '',
+        'id'      => 'mac_hero_already_booked',
+        'type'    => 'text_medium',
         'default' => false,
-    ),
-    array(
-        'name' => 'Hero Already Booked URL',
-        'desc' => '',
-        'id' => 'mac_hero_already_booked_url',
-        'type' => 'text_medium',
+    ],
+    [
+        'name'    => 'Hero Already Booked URL',
+        'desc'    => '',
+        'id'      => 'mac_hero_already_booked_url',
+        'type'    => 'text_medium',
         'default' => false,
-    ),
-    array(
-        'name' => 'Mailing List Call to Action',
-        'desc' => '',
-        'id' => 'mac_mailing_list_cta',
-        'type' => 'textarea_small',
+    ],
+    [
+        'name'    => 'Mailing List Call to Action',
+        'desc'    => '',
+        'id'      => 'mac_mailing_list_cta',
+        'type'    => 'textarea_small',
         'default' => false,
-    ),
-    array(
-        'name' => 'Link to Facebook Page',
-        'desc' => '',
-        'id' => 'mac_facebook_url',
-        'type' => 'text_url',
+    ],
+    [
+        'name'    => 'Link to Facebook Page',
+        'desc'    => '',
+        'id'      => 'mac_facebook_url',
+        'type'    => 'text_url',
         'default' => false,
-    ),
-    array(
-        'name' => 'Link to Twitter',
-        'desc' => '',
-        'id' => 'mac_twitter_url',
-        'type' => 'text_url',
+    ],
+    [
+        'name'    => 'Link to Twitter',
+        'desc'    => '',
+        'id'      => 'mac_twitter_url',
+        'type'    => 'text_url',
         'default' => false,
-    ),
-    array(
-        'name' => 'Link to RSS feed',
-        'desc' => '',
-        'id' => 'mac_feed_url',
-        'type' => 'text_url',
+    ],
+    [
+        'name'    => 'Link to RSS feed',
+        'desc'    => '',
+        'id'      => 'mac_feed_url',
+        'type'    => 'text_url',
         'default' => false,
-    ),
-    array(
+    ],
+    [
         'name' => 'Artist Settings',
         'desc' => '',
         'type' => 'title',
-        'id' => 'mac_artist_settings'
-    ),
-    array(
+        'id'   => 'mac_artist_settings',
+    ],
+    [
         'name' => 'Talent Section Header',
-        'id' => 'mac_talent_header',
+        'id'   => 'mac_talent_header',
         'type' => 'textarea_small',
-    ),
-    array(
+    ],
+    [
         'name' => 'Enable More To Come',
-        'id' => 'mac_enable_more',
+        'id'   => 'mac_enable_more',
         'type' => 'checkbox',
-    ),
-    array(
+    ],
+    [
         'name' => 'Contact Settings',
         'desc' => '',
         'type' => 'title',
-        'id' => 'mac_contact_settings'
-    ),
-    array(
+        'id'   => 'mac_contact_settings',
+    ],
+    [
         'name' => 'General Questions Header',
-        'id' => 'mac_general_questions_header',
+        'id'   => 'mac_general_questions_header',
         'type' => 'text_medium',
-    ),
-    array(
+    ],
+    [
         'name' => 'General Questions Address',
-        'id' => 'mac_general_questions_address_header',
+        'id'   => 'mac_general_questions_address_header',
         'type' => 'text_medium',
-    ),
-    array(
+    ],
+    [
         'name' => 'Booking Questions Header',
-        'id' => 'mac_booking_questions_header',
+        'id'   => 'mac_booking_questions_header',
         'type' => 'text_medium',
-    ),
-    array(
+    ],
+    [
         'name' => 'Booking Questions Address',
-        'id' => 'mac_booking_questions_address_header',
+        'id'   => 'mac_booking_questions_address_header',
         'type' => 'text_medium',
-    ),
-    array(
+    ],
+    [
         'name' => 'Phone Questions Header',
-        'id' => 'mac_phone_questions_header',
+        'id'   => 'mac_phone_questions_header',
         'type' => 'text_medium',
-    ),
-    array(
+    ],
+    [
         'name' => 'Phone Questions Number',
-        'id' => 'mac_phone_questions_address_header',
+        'id'   => 'mac_phone_questions_address_header',
         'type' => 'text_medium',
-    ),
-    array(
+    ],
+    [
         'name' => 'Map Settings',
         'desc' => '',
         'type' => 'title',
-        'id' => 'mac_map_settings'
-    ),
-    array(
+        'id'   => 'mac_map_settings',
+    ],
+    [
         'name' => 'Map Info Copy (narrow-width only)',
-        'id' => 'mac_map_copy',
+        'id'   => 'mac_map_copy',
         'type' => 'textarea_small',
-    ),
-    array(
+    ],
+    [
         'name' => 'News Settings',
         'desc' => '',
         'type' => 'title',
-        'id' => 'mac_news_settings'
-    ),
-    array(
+        'id'   => 'mac_news_settings',
+    ],
+    [
         'name' => 'News Header',
-        'id' => 'mac_news_header',
+        'id'   => 'mac_news_header',
         'type' => 'textarea_small',
-    ),
-    array(
+    ],
+    [
         'name' => 'News "View All" Copy',
-        'id' => 'mac_news_view_all',
+        'id'   => 'mac_news_view_all',
         'type' => 'text_medium',
-    ),
-    array(
+    ],
+    [
         'name' => 'News "View All" URL',
-        'id' => 'mac_news_view_all_url',
+        'id'   => 'mac_news_view_all_url',
         'type' => 'text_medium',
-    ),
-    array(
+    ],
+    [
         'name' => 'FAQ Settings',
         'desc' => '',
         'type' => 'title',
-        'id' => 'mac_faq_settings'
-    ),
-    array(
-        'name' => 'FAQ Categories',
-        'desc' => 'pipe delimited categories for faqs, in order!',
-        'id' => 'mac_piped_cats',
-        'type' => 'textarea_code',
+        'id'   => 'mac_faq_settings',
+    ],
+    [
+        'name'    => 'FAQ Categories',
+        'desc'    => 'pipe delimited categories for faqs, in order!',
+        'id'      => 'mac_piped_cats',
+        'type'    => 'textarea_code',
         'default' => 'What the Heck?|Booking Questions|I’m Booked! Now What?|Managing my Booking',
-    ),
-    array(
+    ],
+    [
         'name' => 'Footer Settings',
         'desc' => '',
         'type' => 'title',
-        'id' => 'mac_footer_settings'
-    ),
-    array(
+        'id'   => 'mac_footer_settings',
+    ],
+    [
         'name' => 'Footer Text',
-        'id' => 'mac_footer_text',
+        'id'   => 'mac_footer_text',
         'type' => 'textarea_small',
-    ),
-);
+    ],
+];
 ?>
