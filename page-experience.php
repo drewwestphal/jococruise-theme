0547df0eb93f5bdb88f96500f05dcfd91aa669d9
Drew Westphal <drewwestphal@users.noreply.github.com>
2016-01-21 23:33:35 -0500

    make droppable artist section

--- page-experience.php
+++ page-experience.php
@@ -28,42 +28,45 @@ $shadowCruisePost = $context['shadowCruisePost'] = $exp_pcs_byname['exp-communit
 $photoGalleryPost = $context['photoGalleryPost'] = $exp_pcs_byname['exp-photos-gallery'];
 $moreInfoPost = $context['moreInfoPost'] = $exp_pcs_byname['exp-more-info'];
 
-$artistMetaQuery = array_map(function ($artistFieldDefinition) {
-    return [
-        // name is the name of the field in
-        // the acf db (e.g. artist type 2015, 2016... blah blah
-        'key'   => $artistFieldDefinition['name'],
-        // we want artists and featured artists
-        'value' => [
-            'artist',
-            'featured artist',
-        ],
-    ];
-}, // we want everything but the first item (which is the current year)
-    array_slice($_ENV['cc_artist_type_and_year_fields_desc'], 1));
+$showArtistSection = false;
+if($showArtistSection) {
+    $artistMetaQuery = array_map(function ($artistFieldDefinition) {
+        return [
+            // name is the name of the field in
+            // the acf db (e.g. artist type 2015, 2016... blah blah
+            'key'   => $artistFieldDefinition['name'],
+            // we want artists and featured artists
+            'value' => [
+                'artist',
+                'featured artist',
+            ],
+        ];
+    }, // we want everything but the first item (which is the current year)
+        array_slice($_ENV['cc_artist_type_and_year_fields_desc'], 1));
 
 // we want artists that were here for any
 // of the target years
-$artistMetaQuery['relation'] = 'OR';
+    $artistMetaQuery['relation'] = 'OR';
 
-$allPastArtists = Timber::get_posts(
-    [
-        'post_type'      => 'artist',
-        'posts_per_page' => -1,
-        'meta_query'     => $artistMetaQuery,
-    ], 'ExperiencePiecePost'
-);
+    $allPastArtists = Timber::get_posts(
+        [
+            'post_type'      => 'artist',
+            'posts_per_page' => -1,
+            'meta_query'     => $artistMetaQuery,
+        ], 'ExperiencePiecePost'
+    );
 
-usort($allPastArtists, [
-    ExperiencePiecePost::class,
-    'artistSortFunction',
-]);
+    usort($allPastArtists, [
+        ExperiencePiecePost::class,
+        'artistSortFunction',
+    ]);
 
-$context['past_artists'] = $allPastArtists;
-$leftCount = ceil(count($allPastArtists) / 2);
-$context['past_artists_left'] = array_slice($allPastArtists, 0, $leftCount);
-$context['past_artists_right'] = array_slice($allPastArtists, $leftCount);
-
+    $context['past_artists'] = $allPastArtists;
+    $leftCount = ceil(count($allPastArtists) / 2);
+    $context['past_artists_left'] = array_slice($allPastArtists, 0, $leftCount);
+    $context['past_artists_right'] = array_slice($allPastArtists, $leftCount);
+}
+$context['show_artist_section'] = $showArtistSection;
 
 $context['featured_events'] = Timber::get_posts(
     [
