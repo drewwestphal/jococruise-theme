7886e964330925418872f53e975ddc5584f347fc
Drew Westphal <drewwestphal@users.noreply.github.com>
2016-01-21 23:24:37 -0500

    fancy header improvements

--- template-fancy-header.php
+++ template-fancy-header.php
@@ -1,8 +1,9 @@
 <?php
-/* Template Name: Fancy What-Is Header Page */
+/* Template Name: Fancy Header Page Without Boat */
 
 $context = Timber::get_context();
 $context['post'] = Timber::get_post(false, 'JoCoCruisePost');
+$context['show_boat'] = isset($showBoat) ? $showBoat : false;
 
 Timber::render('fancy-header-page.twig', $context);
 
