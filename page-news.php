3b3f7299db8ef0eb4a45cfaba2d70c9f44f74894
David Mackenzie <davidamackenzie@gmail.com>
2016-07-28 17:04:44 -0400

    News page timber-ified

--- page-news.php
+++ page-news.php
@@ -1,30 +1,18 @@
-<?php get_header(); ?>
-<?php include 'bumper_top.php'; ?>
-<div class="container nav-spacer">
-	<div class="col-xs-12">
-		<h1>News</h1>
-	    <?php if (have_posts()) : while (have_posts()) : the_post();?>
-            <p><?php the_content(); ?></p>
-	    <?php endwhile; endif; ?>
-		<?php 
-		$args = array(
-			'post_type' => 'post'
-		);
-		$post_query = new WP_Query( $args );
-		if ( $post_query->have_posts() ) {
-			while ( $post_query->have_posts() ) {
-				$post_query->the_post();
-				?>
-					<article class="news-page-item">
-						<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-					<?php if ($post->post_content) {?>
-						<div><?php the_content(); ?></div>
-					<?php  } else { ?>
-						<div><?php the_excerpt(); ?></div>
-					<?php  }; ?>	
-					</article>
-			<?php  }; ?>	
-		<?php  }; ?>
-	</div>
-</div>
-<?php get_footer(); ?>	
+<?php
+use \Timber\Timber;
+$context = Timber::get_context();
+$context['post'] = new JoCoCruisePost();
+$args = array('post_type' => 'post',
+	'posts_per_page'      => 5,
+	'paged' 			  => $paged,
+	'ignore_sticky_posts' => 1);
+/* THIS LINE IS CRUCIAL */
+/* in order for WordPress to know what to paginate */
+/* your args have to be the defualt query */
+query_posts($args);
+/* make sure you've got query_posts in your .php file */
+$context['news_posts'] = $news_posts = Timber::get_posts($args, 'JoCoCruisePost');
+$context['pagination'] = Timber::get_pagination();
+Timber::render('news.twig', $context);
+return;
+?>
