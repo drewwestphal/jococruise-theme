8f137ec72c3455345a9dc5d6073d1662a7d27878
David Mackenzie <davidamackenzie@gmail.com>
2016-07-29 12:41:32 -0400

    Cleaned page.php

--- page.php
+++ page.php
@@ -1,24 +1,6 @@
-<?php if(is_front_page()) { ?>
-    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <header class="header">
-                <h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
-            </header>
-            <section class="entry-content">
-                <?php if(has_post_thumbnail()) {
-                    the_post_thumbnail();
-                } ?>
-                <?php the_content(); ?>
-                <div class="entry-links"><?php wp_link_pages(); ?></div>
-            </section>
-        </article>
-        <?php if(!post_password_required()) {
-            comments_template('', true);
-        } ?>
-    <?php endwhile; endif; ?>
-    </section>
-    <?php get_sidebar(); ?>
-    <?php get_footer();
+<?php if(is_front_page()) {
+
+    include('index.php');
 
 } elseif(is_page('Artists')) {
 
@@ -42,21 +24,5 @@
 
 } else {
     // regular old page style
-    ?>
-
-
-    <section class="joco-page" id="page-page">
-        <?php include 'bumper_top.php'; ?>
-        <div class="container nav-spacer">
-            <div class="col-xs-12 col-lg-10 col-lg-offset-1">
-                <h1><?php the_title() ?></h1>
-                <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-                    <p><?php the_content(); ?></p>
-                <?php endwhile; endif; ?>
-            </div>
-        </div>
-        <?php include 'bumper_bottom.php'; ?>
-        <?php get_footer(); ?>
-    </section>
-
-<?php }; ?>
+    include('single.php');
+}
