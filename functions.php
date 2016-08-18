d004fb284f7d1205f5843fc97f8cd83f413cc7fb
David Mackenzie <davidamackenzie@gmail.com>
2016-08-18 14:38:56 -0400

    Move experience js to twig template and cut down

--- functions.php
+++ functions.php
@@ -10,10 +10,10 @@ add_action('after_setup_theme', function () {
     add_theme_support('post-thumbnails');
 });
 
-require_once __DIR__ . '/include/tgm.php';
+require_once(__DIR__ . '/include/tgm.php');
 require_once(__DIR__ . '/include/cpt.php');
 require_once(__DIR__ . '/include/columns.php');
-require_once __DIR__ . '/vendor/autoload.php';
+require_once(__DIR__ . '/vendor/autoload.php');
 require_once(__DIR__ . '/include/timber_setup.php');
 require_once(__DIR__ . '/include/slick-gallery.php');
 require_once(__DIR__ . '/include/JoCoCruisePost.php');
