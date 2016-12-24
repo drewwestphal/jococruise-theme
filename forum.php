eeaaa52706abf7f4716550042f965080433a0a95
drewwestphal <drewwestphal@users.noreply.github.com>
2016-12-23 19:22:25 -0500

    basic login stuff

--- forum.php
+++ forum.php
@@ -1,6 +1,8 @@
 <?php
 use \Timber\Timber;
 $context = Timber::get_context();
+$context['is_guest'] = !is_user_logged_in();
+$context['login_return'] = wp_login_url($_SERVER['REQUEST_URI']);
 Timber::render('forum.twig', $context);
 return;
 ?>
