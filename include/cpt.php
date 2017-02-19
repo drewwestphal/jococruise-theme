22384a314f2ec4ce2bd8dcf7f906f3b9f2fd5f3f
Drew Westphal <drewwestphal@users.noreply.github.com>
2017-02-19 09:08:19 -0800

    autofmt cpt file

--- include/cpt.php
+++ include/cpt.php
@@ -1,22 +1,22 @@
 <?php
 add_action('init', 'cptui_register_my_cpt_artist');
 function cptui_register_my_cpt_artist() {
-    register_post_type('artist', array(
-        'label' => 'Artist',
-        'description' => '',
-        'public' => true,
+    register_post_type('artist', [
+        'label'              => 'Artist',
+        'description'        => '',
+        'public'             => true,
         'publicly_queryable' => true,
-        'show_ui' => true,
-        'show_in_menu' => true,
-        'capability_type' => 'post',
-        'map_meta_cap' => true,
-        'hierarchical' => false,
-        'rewrite' => array(
-            'slug' => 'artist',
-            'with_front' => true
-        ),
-        'query_var' => true,
-        'supports' => array(
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'capability_type'    => 'post',
+        'map_meta_cap'       => true,
+        'hierarchical'       => false,
+        'rewrite'            => [
+            'slug'       => 'artist',
+            'with_front' => true,
+        ],
+        'query_var'          => true,
+        'supports'           => [
             'title',
             'editor',
             'excerpt',
@@ -28,44 +28,44 @@ function cptui_register_my_cpt_artist() {
             'author',
             'page-attributes',
             'post-formats',
-            'wpcom-markdown'
-        ),
-        'labels' => array(
-            'name' => 'Artist',
-            'singular_name' => 'Artist',
-            'menu_name' => 'Artist',
-            'add_new' => 'Add Artist',
-            'add_new_item' => 'Add New Artist',
-            'edit' => 'Edit',
-            'edit_item' => 'Edit Artist',
-            'new_item' => 'New Artist',
-            'view' => 'View Artist',
-            'view_item' => 'View Artist',
-            'search_items' => 'Search Artist',
-            'not_found' => 'No Artist Found',
+            'wpcom-markdown',
+        ],
+        'labels'             => [
+            'name'               => 'Artist',
+            'singular_name'      => 'Artist',
+            'menu_name'          => 'Artist',
+            'add_new'            => 'Add Artist',
+            'add_new_item'       => 'Add New Artist',
+            'edit'               => 'Edit',
+            'edit_item'          => 'Edit Artist',
+            'new_item'           => 'New Artist',
+            'view'               => 'View Artist',
+            'view_item'          => 'View Artist',
+            'search_items'       => 'Search Artist',
+            'not_found'          => 'No Artist Found',
             'not_found_in_trash' => 'No Artist Found in Trash',
-            'parent' => 'Parent Artist',
-        )
-    ));
+            'parent'             => 'Parent Artist',
+        ],
+    ]);
 }
 
 add_action('init', 'cptui_register_my_cpt_faq');
 function cptui_register_my_cpt_faq() {
-    register_post_type('faq', array(
-        'label' => 'FAQ',
-        'description' => '',
-        'public' => true,
-        'show_ui' => true,
-        'show_in_menu' => true,
+    register_post_type('faq', [
+        'label'           => 'FAQ',
+        'description'     => '',
+        'public'          => true,
+        'show_ui'         => true,
+        'show_in_menu'    => true,
         'capability_type' => 'post',
-        'map_meta_cap' => true,
-        'hierarchical' => false,
-        'rewrite' => array(
-            'slug' => 'faq',
-            'with_front' => true
-        ),
-        'query_var' => true,
-        'supports' => array(
+        'map_meta_cap'    => true,
+        'hierarchical'    => false,
+        'rewrite'         => [
+            'slug'       => 'faq',
+            'with_front' => true,
+        ],
+        'query_var'       => true,
+        'supports'        => [
             'title',
             'editor',
             'excerpt',
@@ -77,44 +77,44 @@ function cptui_register_my_cpt_faq() {
             'author',
             'page-attributes',
             'post-formats',
-            'wpcom-markdown'
-        ),
-        'labels' => array(
-            'name' => 'FAQ',
-            'singular_name' => 'FAQ',
-            'menu_name' => 'FAQ',
-            'add_new' => 'Add FAQ',
-            'add_new_item' => 'Add New FAQ',
-            'edit' => 'Edit',
-            'edit_item' => 'Edit FAQ',
-            'new_item' => 'New FAQ',
-            'view' => 'View FAQ',
-            'view_item' => 'View FAQ',
-            'search_items' => 'Search FAQ',
-            'not_found' => 'No FAQ Found',
+            'wpcom-markdown',
+        ],
+        'labels'          => [
+            'name'               => 'FAQ',
+            'singular_name'      => 'FAQ',
+            'menu_name'          => 'FAQ',
+            'add_new'            => 'Add FAQ',
+            'add_new_item'       => 'Add New FAQ',
+            'edit'               => 'Edit',
+            'edit_item'          => 'Edit FAQ',
+            'new_item'           => 'New FAQ',
+            'view'               => 'View FAQ',
+            'view_item'          => 'View FAQ',
+            'search_items'       => 'Search FAQ',
+            'not_found'          => 'No FAQ Found',
             'not_found_in_trash' => 'No FAQ Found in Trash',
-            'parent' => 'Parent FAQ',
-        )
-    ));
+            'parent'             => 'Parent FAQ',
+        ],
+    ]);
 }
 
 add_action('init', 'cptui_register_my_cpt_sponsors');
 function cptui_register_my_cpt_sponsors() {
-    register_post_type('sponsor', array(
-        'label' => 'sponsor',
-        'description' => '',
-        'public' => true,
-        'show_ui' => true,
-        'show_in_menu' => true,
+    register_post_type('sponsor', [
+        'label'           => 'sponsor',
+        'description'     => '',
+        'public'          => true,
+        'show_ui'         => true,
+        'show_in_menu'    => true,
         'capability_type' => 'post',
-        'map_meta_cap' => true,
-        'hierarchical' => false,
-        'rewrite' => array(
-            'slug' => 'sponsor',
-            'with_front' => true
-        ),
-        'query_var' => true,
-        'supports' => array(
+        'map_meta_cap'    => true,
+        'hierarchical'    => false,
+        'rewrite'         => [
+            'slug'       => 'sponsor',
+            'with_front' => true,
+        ],
+        'query_var'       => true,
+        'supports'        => [
             'title',
             'editor',
             'excerpt',
@@ -126,44 +126,44 @@ function cptui_register_my_cpt_sponsors() {
             'author',
             'page-attributes',
             'post-formats',
-            'wpcom-markdown'
-        ),
-        'labels' => array(
-            'name' => 'Sponsors',
-            'singular_name' => 'Sponsor',
-            'menu_name' => 'Sponsors',
-            'add_new' => 'Add Sponsor',
-            'add_new_item' => 'Add New Sponsor',
-            'edit' => 'Edit',
-            'edit_item' => 'Edit Sponsor',
-            'new_item' => 'New Sponsor',
-            'view' => 'View Sponsors',
-            'view_item' => 'View Sponsors',
-            'search_items' => 'Search Sponsors',
-            'not_found' => 'No Sponsors Found',
+            'wpcom-markdown',
+        ],
+        'labels'          => [
+            'name'               => 'Sponsors',
+            'singular_name'      => 'Sponsor',
+            'menu_name'          => 'Sponsors',
+            'add_new'            => 'Add Sponsor',
+            'add_new_item'       => 'Add New Sponsor',
+            'edit'               => 'Edit',
+            'edit_item'          => 'Edit Sponsor',
+            'new_item'           => 'New Sponsor',
+            'view'               => 'View Sponsors',
+            'view_item'          => 'View Sponsors',
+            'search_items'       => 'Search Sponsors',
+            'not_found'          => 'No Sponsors Found',
             'not_found_in_trash' => 'No Sponsors Found in Trash',
-            'parent' => 'Parent Sponsor',
-        )
-    ));
+            'parent'             => 'Parent Sponsor',
+        ],
+    ]);
 }
 
 add_action('init', 'cptui_register_my_cpt_cities');
 function cptui_register_my_cpt_cities() {
-    register_post_type('city', array(
-        'label' => 'city',
-        'description' => '',
-        'public' => true,
-        'show_ui' => true,
-        'show_in_menu' => true,
+    register_post_type('city', [
+        'label'           => 'city',
+        'description'     => '',
+        'public'          => true,
+        'show_ui'         => true,
+        'show_in_menu'    => true,
         'capability_type' => 'post',
-        'map_meta_cap' => true,
-        'hierarchical' => false,
-        'rewrite' => array(
-            'slug' => 'city',
-            'with_front' => true
-        ),
-        'query_var' => true,
-        'supports' => array(
+        'map_meta_cap'    => true,
+        'hierarchical'    => false,
+        'rewrite'         => [
+            'slug'       => 'city',
+            'with_front' => true,
+        ],
+        'query_var'       => true,
+        'supports'        => [
             'title',
             'editor',
             'excerpt',
@@ -175,25 +175,25 @@ function cptui_register_my_cpt_cities() {
             'author',
             'page-attributes',
             'post-formats',
-            'wpcom-markdown'
-        ),
-        'labels' => array(
-            'name' => 'Cities',
-            'singular_name' => 'City',
-            'menu_name' => 'Cities',
-            'add_new' => 'Add City',
-            'add_new_item' => 'Add New City',
-            'edit' => 'Edit',
-            'edit_item' => 'Edit City',
-            'new_item' => 'New City',
-            'view' => 'View Cities',
-            'view_item' => 'View Cities',
-            'search_items' => 'Search Cities',
-            'not_found' => 'No Cities Found',
+            'wpcom-markdown',
+        ],
+        'labels'          => [
+            'name'               => 'Cities',
+            'singular_name'      => 'City',
+            'menu_name'          => 'Cities',
+            'add_new'            => 'Add City',
+            'add_new_item'       => 'Add New City',
+            'edit'               => 'Edit',
+            'edit_item'          => 'Edit City',
+            'new_item'           => 'New City',
+            'view'               => 'View Cities',
+            'view_item'          => 'View Cities',
+            'search_items'       => 'Search Cities',
+            'not_found'          => 'No Cities Found',
             'not_found_in_trash' => 'No Cities Found in Trash',
-            'parent' => 'Parent Cities',
-        )
-    ));
+            'parent'             => 'Parent Cities',
+        ],
+    ]);
 }
 
 /**
@@ -203,26 +203,26 @@ function cptui_register_my_cpt_cities() {
  *
  */
 // dw custom
-add_action('init', function() {
+add_action('init', function () {
     // featured events
-    register_post_type('featured-event', array(
-        'label' => 'featured-event',
-        'description' => '',
-        'show_ui' => true,
-        'show_in_menu' => true,
-        'capability_type' => 'post',
-        'map_meta_cap' => true,
+    register_post_type('featured-event', [
+        'label'              => 'featured-event',
+        'description'        => '',
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'capability_type'    => 'post',
+        'map_meta_cap'       => true,
         // these apparently block front end visibility
-        'hierarchical' => false,
-        'public' => false,
-        'has_archive' => false,
+        'hierarchical'       => false,
+        'public'             => false,
+        'has_archive'        => false,
         'publicly_queryable' => false,
-        'rewrite' => array(
-            'slug' => 'featured-event',
-            'with_front' => true
-        ),
-        'query_var' => false,
-        'supports' => array(
+        'rewrite'            => [
+            'slug'       => 'featured-event',
+            'with_front' => true,
+        ],
+        'query_var'          => false,
+        'supports'           => [
             'title',
             'editor',
             'excerpt',
@@ -234,25 +234,25 @@ add_action('init', function() {
             'author',
             'page-attributes',
             'post-formats',
-            'wpcom-markdown'
-        ),
-        'labels' => array(
-            'name' => 'Featured Events',
-            'singular_name' => 'Featured Event',
-            'menu_name' => 'Featured Events (Exp. Pg.)',
-            'add_new' => 'Add New Featured Event',
-            'add_new_item' => 'Add New Featured Event',
-            'edit' => 'Edit',
-            'edit_item' => 'Edit Featured Event',
-            'new_item' => 'New Featured Event',
-            'view' => 'View Featured Events',
-            'view_item' => 'View Featured Event',
-            'search_items' => 'Search Featured Events',
-            'not_found' => 'No Featured Events Found',
+            'wpcom-markdown',
+        ],
+        'labels'             => [
+            'name'               => 'Featured Events',
+            'singular_name'      => 'Featured Event',
+            'menu_name'          => 'Featured Events (Exp. Pg.)',
+            'add_new'            => 'Add New Featured Event',
+            'add_new_item'       => 'Add New Featured Event',
+            'edit'               => 'Edit',
+            'edit_item'          => 'Edit Featured Event',
+            'new_item'           => 'New Featured Event',
+            'view'               => 'View Featured Events',
+            'view_item'          => 'View Featured Event',
+            'search_items'       => 'Search Featured Events',
+            'not_found'          => 'No Featured Events Found',
             'not_found_in_trash' => 'No Featured Events Found in Trash',
-            'parent' => 'Parent Featured Events',
-        )
-    ));
+            'parent'             => 'Parent Featured Events',
+        ],
+    ]);
 });
 
 ?>
