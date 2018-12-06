33a60135140f52b81ff6597e3d96fb7bbb2237bd
Drew Westphal <drewwestphal@users.noreply.github.com>
2018-12-05 19:15:27 -0500

    cors header attempt

--- emptiness.php
+++ emptiness.php
@@ -1,6 +1,8 @@
 <?php
 /* Template Name: Emptiness (for iframes, etc) */
 $post = new JoCoCruisePost();
+header("Access-Control-Allow-Origin: https://jococruise.com");
+
 echo $post->content();
 exit();
 ?>
